b02bb2f42d8223ced0480f32f5e4715eb86d2a59
CodyFenison <codyfenison@gmail.com>
2021-10-05 23:16:44 -0400

    Project complete

---
+++ Assets/Scripts/EnemyPatrolOnly.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyPatrolOnly : MonoBehaviour
+{
+    Transform target;
+    NavMeshAgent agent;
+    int currentNode;
+    int previousNode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentNode = Random.Range(2, GameManager.gm.nodes.Length);
+        previousNode = currentNode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Patrol();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "node")
+        {
+            currentNode = Random.Range(2, GameManager.gm.nodes.Length);
+            while(currentNode == previousNode)
+            {
+                currentNode = Random.Range(2, GameManager.gm.nodes.Length);
+            }
+            previousNode = currentNode;
+        }
+    }
+
+    void Patrol()
+    {
+
+        agent.destination = GameManager.gm.nodes[currentNode].position;
+    }
+}
